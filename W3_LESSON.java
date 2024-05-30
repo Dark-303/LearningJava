2a8fcb2f59ac3dcb2cb45133e47d1376df90d25d
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2024-05-30 14:24:55 -0400

    W3 Course
    Lessons 1 and 2:
    Printed "Hello World"
    Learned about Main function

---
+++ W3_LESSON.java
@@ -0,0 +1,11 @@
+// Lesson 1: Hello World
+/* public class W3_LESSON {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  } */
+
+// Lesson 2: Main function
+/*public static void main(String[] args) {
+  System.out.println("Hello World");
+}*/
