9361d22e52efdd77137c05114b4248a7fdd21d04
Dark-303 <156324532+Dark-303@users.noreply.github.com>
2024-06-03 15:46:42 -0400

    Learning lessons 2 - 9
    👍

--- W3_LESSON.java
+++ W3_LESSON.java
@@ -6,6 +6,112 @@
   } */
 
 // Lesson 2: Main function
-/*public static void main(String[] args) {
-  System.out.println("Hello World");
+/*public class W3_LESSON {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }*/
+
+// Lesson 3: print vs. println
+/*public class W3_LESSON {
+  public static void main(String[] args) {
+    System.out.println("This will print on the first line.");
+    System.out.println("This will print on the second line. ");
+    System.out.print("This will print on the third line. ");
+    System.out.print("This will print on the same line (the third one). ");
+  }
+}*/
+
+// Lesson 4: Print numbers
+// Same as python. It can print as a str, int, or float.
+// If a mathematical equation is printed it prints the result.
+
+// Lesson 5: Comments (lol)
+// This is a single line comment
+/*
+ This is a multi-line comment
+ as you can see.
+*/
+//there is an opening section "/*"
+//and a closing section "*/"
+
+// Lesson 6: Variables
+// format: type variableName = value
+/*public class W3_LESSON {
+  public static void main(String [] args) {
+    // Declaring and assigning in one line
+      // String variable
+      String Letters = "abc";
+      System.out.println(Letters);
+
+      // Integer variables
+        // Integer variable declaration
+        Integer Numbers = 123;
+        System.out.println(Numbers);
+
+        // Alternate declaration of integer variable
+        int Num = 123;
+        System.out.println(Num);
+    
+      // Final variables (value cannot be changed after assigned)
+      final String YouCantChangeMe = "I am unchangable";
+      // The line below makes an error
+      //YouCantChangeMe = "CHANGE";
+      System.out.println(YouCantChangeMe);
+    
+    // Declaring then later assigning in two lines
+    String ABC;
+    ABC = "abc";
+    System.out.println(ABC);
+
+    // There are other variable types. Here are all the types:
+    int myNum = 1;
+    float myFloatNum = 1.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hi";
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}*/
+
+// Lesson 7: Displaying Variables
+// println is commonly used to display variables
+/*public class W3_LESSON {
+  public static void main(String [] args) {
+    // Simply displaying this variable in the console
+    String myString = "Sussy Amogus";
+    System.out.println(myString);
+
+    // Strings and string variables can be combined
+    String Name = "Bob";
+    System.out.println("Hello " + Name);
+
+    // You can create math operations using variables
+    int x = 1;
+    int y = 1;
+    System.out.println(x + y);
+
+    // Strings can be joined together
+    String Word1 = "Hello ";
+    String Word2 = "World!";
+    System.out.println(Word1 + Word2);
+  }
+}*/
+
+// Lesson 8: Java Identifiers
+// give your variables unique names 👍
+// Naming rules:
+// - Names can contain letters, digits, underscores, and dollar signs
+// - Names must begin with a letter
+// - Names should start with a lowercase letter, and cannot contain whitespace
+// - Names can also begin with $ and _ (but we will not use it in this tutorial)
+// - Names are case-sensitive ("myVar" and "myvar" are different variables)
+// - Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+
+// Lesson 9: Variable Examples
+// No point for me to do this (takes too long)
+
